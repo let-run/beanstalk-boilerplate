8dbea95e3b720b8b8068ce63eed1836cc2dce521
Vadym Popov <me@vpopov.org>
2023-03-02 21:34:14 +0100

    re-test

--- .platform/hooks/postdeploy/01_certbot_run.sh
+++ .platform/hooks/postdeploy/01_certbot_run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-certbot run --nginx --non-interactive --agree-tos --preferred-challenges http --email vadym@let.run -d eb.let.run
+certbot run --nginx --non-interactive --agree-tos --preferred-challenges http --email vadym@let.run -d bean.let.run
